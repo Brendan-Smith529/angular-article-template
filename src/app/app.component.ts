b791875e63f0cab3b0f3ac06f452338ed456cff5
Brendan <brendansmith5292002@gmail.com>
2024-12-19 15:09:17 -0500

    Initial site layout; beginning of theme switching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  NgZone,
+  OnDestroy,
+  QueryList,
+  ViewChildren
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from './shared/shared.module';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    CommonModule,
+    SharedModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnDestroy {
   title = 'personal-site';
+
+  scrollToSection(e: MouseEvent, id: string): void {
+    const section = document.getElementById(id);
+    e.preventDefault();
+
+    if (section)
+      section.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  currentSection: string = ''
+  @ViewChildren('section') sections!: QueryList<ElementRef>
+  private observer!: IntersectionObserver
+
+  constructor(private ngZone: NgZone) {}
+
+  ngAfterViewInit(): void {
+    const observerOptions = {
+			root: null,
+			rootMargin: "0px",
+			threshold: 0.4,
+		};
+
+		this.observer = new IntersectionObserver((entries) => {
+      this.ngZone.run(() =>
+        this.currentSection = entries
+          .find(entry => entry.isIntersecting)!
+          .target
+          .getAttribute("aria-labelledby") as string
+      )
+		}, observerOptions);
+
+		for (const section of this.sections)
+			this.observer.observe(section.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    for (const section of this.sections)
+      this.observer.unobserve(section.nativeElement);
+  }
 }
