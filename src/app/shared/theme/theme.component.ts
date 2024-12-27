ae5838b2dff637d45a79f02b9866093950a4693f
Brendan <brendansmith5292002@gmail.com>
2024-12-27 00:22:54 -0500

    Basic theme selector working

--- src/app/shared/theme/theme.component.ts
+++ src/app/shared/theme/theme.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-theme',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './theme.component.html',
   styleUrl: './theme.component.css'
 })
 export class ThemeComponent {
+  @ViewChild('menuContainer') menuContainer!: ElementRef;
+  private type: string;
+  showThemeOpts: boolean = false;
 
+  themes = [
+    { value: 'os-default', label: 'OS Default' },
+    { value: 'light', label: 'Light' },
+    { value: 'dark', label: 'Dark' },
+  ];
+
+  constructor() {
+    const themeType = localStorage.getItem('theme')
+      ? localStorage.getItem('theme') as string
+      : ''
+
+    this.type = themeType !== '' ? themeType : 'os-default';
+
+    if (themeType !== this.type)
+      localStorage.setItem('theme', this.type);
+  }
+
+  toggleDisplay(): void {
+    this.showThemeOpts = !this.showThemeOpts;
+  }
+
+  getTheme(): string {
+    return 'icon-' + this.type;
+  }
+
+  setTheme(type: string): void {
+    localStorage.setItem('theme', type);
+    this.type = type;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onOutsideClick(event: MouseEvent): void {
+    if (!this.showThemeOpts || !this.menuContainer)
+      return;
+
+    if (!this.menuContainer.nativeElement.contains(event.target))
+      this.showThemeOpts = false;
+  }
 }
