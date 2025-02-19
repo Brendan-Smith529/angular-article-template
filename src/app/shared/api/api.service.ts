928dae5975b26f4f83eb8d23246cbed5d24dafa2
Brendan <brendansmith5292002@gmail.com>
2025-02-19 15:53:37 -0500

    Make API public; requests capped at 30/day per client

--- src/app/shared/api/api.service.ts
+++ src/app/shared/api/api.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ApiResponse } from './api';
-import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl: string = environment.apiUrl;
-
+  private apiUrl: string = 'https://rsh2vvvpm3.execute-api.us-east-2.amazonaws.com/default/retrieveContent';
   constructor(private http: HttpClient) { }
 
   getContent() {
